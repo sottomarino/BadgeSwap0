6afe81995ac32ec74f16661a7f254b5182ff514a
simone ambrosi <70901023+sottomarino@users.noreply.github.com>
2022-01-28 16:55:01 +0100

    Update my-assets.js

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -53,7 +53,7 @@ export default function MyAssets() {
           {
             nfts.map((nft, i) => (
               <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="rounded" />
+                <image src={nft.image} className="rounded" />
                 <div className="p-4 bg-black">
                   <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
                 </div>
