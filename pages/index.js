b13eac3a7a7a9bb028936b17e0cec31cd5bda98f
simone ambrosi <70901023+sottomarino@users.noreply.github.com>
2022-01-29 05:53:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from "web3modal"
+import Image from 'next/image'
 
 import {
   nftaddress, nftmarketaddress
@@ -68,7 +69,7 @@ export default function Home() {
           {
             nfts.map((nft, i) => (
               <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <image src={nft.image} />
+                <Image src={nft.image} />
                 <div className="p-4">
                   <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
                   <div style={{ height: '70px', overflow: 'hidden' }}>
